747070bc9717d6cbfc9214c64fde0a3838329f56
alx <35928901+alxmel@users.noreply.github.com>
2018-10-31 19:46:54 -0400

    updated fantasy game inventory to include string justification for printing the inventory, as well as minor updates

--- projects/python_projects/fantasy_game_inventory.py
+++ projects/python_projects/fantasy_game_inventory.py
@@ -13,22 +13,35 @@ new_loot = []
 print()
 print('Hello! Welcome to the interactive inventory tracker!')
 
+
+# Defining function that will be used to print out the inventory.
+def display_inventory(inv_items):
+    print('CURRENT INVENTORY'.center(20 + 20, '-'))
+    print()
+    for k, v in inv_items.items():
+        print(k.ljust(30, '.') + str(v).rjust(1))
+    print()
+    print('-'.center(20 + 20, '-'))
+
+
 while True:
     print()
     print('Type "1" to print out your inventory, "2" for updating your inventory, "3" for adding new boss loot (once defeated), or "exit" to quit:')
     choice = input()
-    if choice == 'exit':
+    if choice.lower() == 'exit':
+        print()
         print('Thanks for using the Fantasy Inventory System!')
         break
     elif choice == '1':
         print()    # Blank print statements added for output readability
-        pprint.pprint(inventory, width=1)
+#        pprint.pprint(inventory, width=1) ### Commented out in favor for using string justification to print inventory
+        display_inventory(inventory)
         print()
     elif choice == '2':
         print()
         while True:
             print('Please enter the item name to add or deduce from the inventory ("return" to return to main menu):')
-            key = input()
+            key = input().lower()
             print()
             if key == 'return':
                 break
